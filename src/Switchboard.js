e9ec089b0624aab8805a36d294c200ad7925a9d9
Eduardo Bouças <mail@eduardoboucas.com>
2021-02-23 11:46:19 +0000

    feat: add link to /random in Switchboard

--- src/Switchboard.js
+++ src/Switchboard.js
@@ -8,27 +8,48 @@ function Switchboard() {
 
     window.location.href = `/${orgSlug}`;
   };
+  const ConditionalLink = ({ condition, link, children }) => {
+    if (condition) {
+      return (
+        <a href={link} className="nes-btn is-primary switchboard-control">
+          {children}
+        </a>
+      );
+    }
+
+    return (
+      <span className="nes-btn is-primary switchboard-control is-disabled">
+        {children}
+      </span>
+    );
+  };
 
   return (
     <div className="constrain">
       <form onSubmit={navigate}>
         <div className="switchboard-controls">
-          <label className="switchboard-control" htmlFor="org-slug">
-            github.com/
-          </label>
-          <input
-            id="org-slug"
-            type="text"
-            className="nes-input switchboard-control switchboard-input"
-            onChange={(event) => setOrgSlug(event.target.value)}
-            value={orgSlug}
-          />
-          <a
-            href={`/${orgSlug}`}
-            className="nes-btn is-primary switchboard-control"
-          >
-            Go
-          </a>
+          <div>
+            <label className="switchboard-control" htmlFor="org-slug">
+              github.com/
+            </label>
+            <input
+              autoComplete={false}
+              id="org-slug"
+              type="text"
+              className="nes-input switchboard-control switchboard-input"
+              onChange={(event) => setOrgSlug(event.target.value)}
+              value={orgSlug}
+            />
+          </div>
+
+          <div className="switchboard-buttons">
+            <ConditionalLink condition={orgSlug} link={`/${orgSlug}`}>
+              Catch 'em all
+            </ConditionalLink>
+            <ConditionalLink condition={orgSlug} link={`/${orgSlug}/random`}>
+              Catch one randomly
+            </ConditionalLink>
+          </div>
         </div>
       </form>
     </div>
